337dc0db8ca4d9ff8c7fcd4ae9ebaa054c2bc584
Sudipta <hisudipta26@gmail.com>
2023-09-16 00:35:41 +0530

    feat:search func

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/sigma.png";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const [searchInput, setSearchInput] = useState("");
+
+  const components = [
+    "avatar",
+    "alert",
+    "badge",
+    "button",
+    "card",
+    "headings",
+    "text",
+    "image",
+  ];
+
+  const handleSearch = () => {
+    if (searchInput.trim() !== "") {
+      if (components.includes(searchInput.toLowerCase())) {
+        navigate(`/components/${searchInput.toLowerCase()}`);
+      } else {
+        alert("Sorry, no such component found!");
+      }
+    }
+    setSearchInput("");
+  };
 
   return (
     <nav className="navbar">
       <div className="left-nav" onClick={() => navigate("/")}>
         <img src={logo} alt="logo" />
         <h2>Sigma UI</h2>
+      </div>
+      <div className="middle-nav">
+        <div className="searchbar">
+          <input
+            type="text"
+            placeholder="Search components..."
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          <button onClick={handleSearch}><i className="fas fa-search"></i> Search</button>
+        </div>
       </div>
       <div className="right-nav">
         <a href="/">Home</a>
